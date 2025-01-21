73ab4f79ce37ccedd56bc929a04251ad6bd93845
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-5.local>
2025-01-21 00:11:13 -0500

    update code

--- stat_analysis.py
+++ stat_analysis.py
@@ -5,123 +5,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-vocab = Path('data_gen/base-grammar.gr').read_text().strip().split('\n\n\n')[1]
-with open('vocab/vocab_Y.txt', 'w') as vy:
-    all_vocab = [x.split()[-1] for x in vocab.split('\n') if len(x.strip())>1]
-    vy.write('\n'.join(all_vocab))
 
-# random.seed(50)
-# def get_bigrams(sents):
-#     bigrams = [b for l in sents for b in zip(l.split(" ")[1:-1], l.split(" ")[2:])]
-#     bigram_perm = ['_'.join(list(x)) for x in bigrams if x[1]!='']
-#     return bigram_perm
+random.seed(50)
+def get_bigrams(sents):
+    bigrams = [b for l in sents for b in zip(l.split(" ")[1:-1], l.split(" ")[2:])]
+    bigram_perm = ['_'.join(list(x)) for x in bigrams if x[1]!='']
+    return bigram_perm
+
+def get_bigram_persent(sent):
+    bigrams = [b for b in zip(sent.split(' ')[1:-1], sent.split(' ')[2:])]
+    bigram_perm = ['_'.join(list(x)) for x in bigrams]
+    return bigram_perm
 #
-# def get_bigram_persent(sent):
-#     bigrams = [b for b in zip(sent.split(' ')[1:-1], sent.split(' ')[2:])]
-#     bigram_perm = ['_'.join(list(x)) for x in bigrams]
-#     return bigram_perm
-#
-# clo =["glim flugit",
-# "flairb flugit gentif",
-# "daffin clidam",
-# "glim ka zup",
-# "glim ka tombur bleggin",
-# "flairb ka lapal bleggin gentif",
-# "daffin ka tombur spad",
-# "flairb zup",
-# "daffin lapal nawg",
-# "flairb tombur flugit spad",
-# "flairb lapal spad",
-# "bleggin spad",
-# "ka lapal",
-# "ka lapal flugit",
-# "ka tombur nawg clidam",
-# "ka lapal spad",
-# "lapal nawg",
-# "tombur bleggin gentif",
-# "zup gentif",
-# "glim nawg",
-# "glim mawg spad",
-# "glim clidam",
-# "flairb ka tombur",
-# "flairb ka zup nawg",
-# "daffin ka zup flugit clidam",
-# "glim ka lapal gentif",
-# "daffin lapal",
-# "glim zup bleggin",
-# "daffin zup flugit spad",
-# "daffin tombur clidam",
-# "mawg gentif",
-# "ka zup",
-# "ka zup bleggin",
-# "ka tombur mawg gentif",
-# "ka zup spad",
-# "tombur flugit",
-# "zup mawg clidam",
-# "lapal clidam"]
-#
-#
-# op = ["glim flugit",
-# "flairb flugit gentif",
-# "daffin clidam",
-# "glim tombur ka",
-# "glim lapal ka bleggin",
-# "flairb tombur ka bleggin gentif",
-# "daffin tombur ka spad",
-# "flairb ka",
-# "daffin ka nawg",
-# "flairb ka flugit spad",
-# "flairb ka spad",
-# "bleggin spad",
-# "tombur ka",
-# "lapal ka flugit",
-# "tombur ka mawg clidam",
-# "lapal ka spad",
-# "ka nawg",
-# "ka bleggin gentif",
-# "ka gentif",
-# "glim nawg",
-# "glim mawg spad",
-# "glim clidam",
-# "flairb zup ka",
-# "flairb lapal ka nawg",
-# "daffin zup ka flugit clidam",
-# "glim zup ka gentif",
-# "daffin ka",
-# "glim ka bleggin",
-# "daffin ka flugit spad",
-# "daffin ka clidam",
-# "mawg gentif",
-# "zup ka",
-# "tombur ka bleggin",
-# "zup ka nawg gentif",
-# "lapal ka spad",
-# "ka flugit",
-# "ka mawg clidam",
-# "ka clidam"]
-#
-# grammar_file = 'data_gen/grammar53exp1'
-# grammar_file_pert = 'data_gen/grammar53exp1_permutation'
-# b_a = []
-# b_p = []
-# # with open('same_bigram_sents.txt', 'w') as same, open('same_bigram_sents_pert.txt', 'w') as pe:
-# for i in range(10):
-#
-#     sents = Path(f'{grammar_file}/grammar53exp1/{str(i)}.trn').read_text().strip().split('\n')
-#     sents_pert = Path(f'{grammar_file_pert}/grammar53exp1_permutation/{str(i)}.trn').read_text().strip().split('\n')
-#     # sents = clo
-#     # sents_pert = op
-#     bigrams = len(Counter(get_bigrams(sents)))
-#     bigrams_pert = len(Counter(get_bigrams(sents_pert)))
-#     b_a.append(bigrams)
-#     b_p.append(bigrams_pert)
-#     print(bigrams, bigrams_pert)
-#
-#     counter_a = Counter(get_bigrams(sents_pert))
-#     counter_b = Counter(get_bigrams(sents))
-#
-# print(np.mean(b_a))
-# print(np.mean(b_p))
+
+grammar_file = 'data_gen/grammar/grammar53exp1'
+grammar_file_pert = 'data_gen/grammar/grammar53exp1_permutation'
+b_a = []
+b_p = []
+# with open('same_bigram_sents.txt', 'w') as same, open('same_bigram_sents_pert.txt', 'w') as pe:
+for i in range(10):
+
+    sents = Path(f'{grammar_file}/grammar53exp1/{str(i)}.trn').read_text().strip().split('\n')
+    sents_pert = Path(f'{grammar_file_pert}/grammar53exp1_permutation/{str(i)}.trn').read_text().strip().split('\n')
+    # sents = clo
+    # sents_pert = op
+    bigrams = len(Counter(get_bigrams(sents)))
+    bigrams_pert = len(Counter(get_bigrams(sents_pert)))
+    b_a.append(bigrams)
+    b_p.append(bigrams_pert)
+    print(bigrams, bigrams_pert)
+
+    counter_a = Counter(get_bigrams(sents_pert))
+    counter_b = Counter(get_bigrams(sents))
+
+print(np.mean(b_a))
+print(np.mean(b_p))
 
     # frq = []
     # sent_frq = []
