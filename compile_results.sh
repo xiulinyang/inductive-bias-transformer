956bca50d6ce2ac15d04f810f9cfee064d594717
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-5.local>
2025-01-23 14:33:47 -0500

    update results and code

--- compile_results.sh
+++ compile_results.sh
@@ -1,39 +1,6 @@
 #!/bin/bash
- 
-python compile_results.py -m trans -g gn_grammarexp1_permutation_correct
-python compile_results.py -m trans -g gn_grammarexp1_permutation_incorrect
+python compile_results.py -m trans -o perplexity.csv
+python compile_results.py -m lstm -o perplexity.csv
 
-python compile_results.py -m trans -g gn_grammarexp1_correct
-python compile_results.py -m trans -g gn_grammarexp1_incorrect
-
-python compile_results.py -m trans -g gn_grammarexp1
-python compile_results.py -m trans -g gn_grammarexp1_permutation
-
-
-python compile_results.py -m trans -g gn_grammarexp2_permutation_correct
-python compile_results.py -m trans -g gn_grammarexp2_permutation_incorrect
-
-python compile_results.py -m trans -g gn_grammarexp2_correct
-python compile_results.py -m trans -g gn_grammarexp2_incorrect
-
-python compile_results.py -m trans -g gn_grammarexp2
-python compile_results.py -m trans -g gn_grammarexp2_permutation
-
-python compile_results.py -m trans -g gn_grammarexp3_permutation_correct
-python compile_results.py -m trans -g gn_grammarexp3_permutation_incorrect
-
-python compile_results.py -m trans -g gn_grammarexp3_correct
-python compile_results.py -m trans -g gn_grammarexp3_incorrect
-
-python compile_results.py -m trans -g gn_grammarexp3
-python compile_results.py -m trans -g gn_grammarexp3_permutation
-
-
-python compile_results.py -m trans -g grammar42exp1
-python compile_results.py -m trans -g grammar42exp1_permutation
-
-python compile_results.py -m trans -g grammar41exp1
-python compile_results.py -m trans -g grammar41exp1_permutation
-
-python compile_results.py -m trans -g grammar53exp1
-python compile_results.py -m trans -g grammar53exp1_permutation
+python dep_accuracy.py -m trans -o accuracy.csv
+python dep_accuracy.py -m lstm -o accuracy.csv
