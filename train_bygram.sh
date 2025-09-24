286c282070e6226e4ca4a01acfb05905b38cacbe
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-24 16:37:31 -0400

    update bigram script

--- train_bygram.sh
+++ train_bygram.sh
@@ -5,13 +5,13 @@ mkdir bigram
 
 for num in {0..9}; do
   for grammar in "${all_grammars[@]}"; do
-    bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
-    bin/build_binary bigram/${grammar}_${num}.arpa ${grammar}_${num}.bin
+    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
+    kenlm/build/bin/build_binary bigram/${grammar}_${num}.arpa bigram/${grammar}_${num}.bin
 
-    bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_permutation/${num}/trn.spm > ${grammar}_${num}_permutation.arpa
-    bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_r/${num}/trn.spm > ${grammar}_${num}_r.arpa
-    bin/build_binary bigram/${grammar}_${num}_r.arpa ${grammar}_${num}_r.bin
-    bin/build_binary bigram/${grammar}_${num}_permutation.arpa ${grammar}_${num}_permutation.bin
+    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_permutation/${num}/trn.spm > bigram/${grammar}_${num}_permutation.arpa
+    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_r/${num}/trn.spm > bigram/${grammar}_${num}_r.arpa
+    kenlm/build/bin/build_binary bigram/${grammar}_${num}_r.arpa bigram/${grammar}_${num}_r.bin
+    kenlm/build/bin/build_binary bigram/${grammar}_${num}_permutation.arpa bigram/${grammar}_${num}_permutation.bin
 
   done
 done
