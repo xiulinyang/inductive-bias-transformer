a6e3736111912d922867a2169dfdd6818ba96e00
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-24 20:01:32 +0200

    update scripts

---
+++ run_all_lstm2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p lstm-results
+
+
+for num in {6..9}; do
+  bash train_lm_lstm.sh grammar_close_then_open_3 $num
+  bash train_lm_lstm.sh grammar_close_then_open_3_permutation $num
+  bash train_lm_lstm.sh grammar_close_then_open_30 $num
+  bash train_lm_lstm.sh grammar_close_then_open_30_permutation $num
+  bash train_lm_lstm.sh grammar_close_then_open_60 $num
+  bash train_lm_lstm.sh grammar_close_then_open_60_permutation $num
+  bash train_lm_lstm.sh grammar_close_then_open_3_r $num
+  bash train_lm_lstm.sh grammar_close_then_open_30_r $num
+  bash train_lm_lstm.sh grammar_close_then_open_60_r $num
+  bash train_lm_lstm.sh grammar_close_then_open_3_reverse $num
+  bash train_lm_lstm.sh grammar_close_then_open_3_reverse_permutation $num
+done
