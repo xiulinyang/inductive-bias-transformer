cebbf32285c9d01296556c971f265a1f8578fb82
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-25 08:52:48 -0400

    add n-gram prob for all other perturbations

--- get_n_gram_prob.sh
+++ get_n_gram_prob.sh
@@ -5,7 +5,11 @@ mkdir -p bigram
 
 for num in {0..9}; do
   for grammar in "${all_grammars[@]}"; do
-    python bi_gram.py -g $grammar -s $num -m 'trans'
-    python bi_gram.py -g $grammar -s $num -m 'lstm'
+    python bi_gram.py -g "${grammar}_permutation" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}_permutation" -s $num -m 'lstm'
+    python bi_gram.py -g "${grammar}_r" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}_r" -s $num -m 'lstm'
+    python bi_gram.py -g "${grammar}_reverse" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}_reverse" -s $num -m 'lstm'
   done
 done
