26d71d4e44c057d81f5b4a39e10bd5ed30c1ddb9
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-25 09:40:22 -0400

    train ngram

--- train_bygram.sh
+++ train_bygram.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-all_grammars=("grammar_close_then_open_3" "grammar_close_then_open_30" "grammar_close_then_open_60")
+all_grammars=("grammar_close_then_open_3" "grammar_close_then_open_30" "grammar_close_then_open_60"
+"grammar_close_then_open_3_permutation" "grammar_close_then_open_30_permutation" "grammar_close_then_open_60_permutation"
+ "grammar_close_then_open_3_reverse" "grammar_close_then_open_3_reverse_permutation"
+"grammar_close_then_open_3_r" "grammar_close_then_open_30_r" "grammar_close_then_open_60_r")
 mkdir -p bigram
 
 for num in {0..1}; do
   for grammar in "${all_grammars[@]}"; do
-#    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
-    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}_permutation/${num}/trn.spm > bigram/${grammar}_${num}_permutation.arpa
-#    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}_r/${num}/trn.spm > bigram/${grammar}_${num}_r.arpa
-#    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}_reverse/${num}/trn.spm > bigram/${grammar}_${num}_reverse.arpa
-
+    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
   done
 done
