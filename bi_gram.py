ce71faa1e08913c18921e6162d54e05b62464692
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-24 21:03:07 -0400

    debug

--- bi_gram.py
+++ bi_gram.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 import kenlm
 import math
-
+from pathlib import Path
 parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--grammar_name', type=str, help='name of the grammar, e.g., grammar_close_then_open')
 parser.add_argument('-s', '--sample_split', type=int, help='0-9')
@@ -14,12 +14,13 @@ model_type = args.model_type
 sample_split = str(args.sample_split)
 grammar_name = args.grammar_name
 
-model = kenlm.LanguageModel(f'bigram/{grammar_name}_{sample_split}.arpa')
-test_data = pd.read_csv(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.txt', sep='\t', header=None,
-            names=['sent', 'toks', 'model_prob']).to_dict(orient='records')
 
+# test_data = pd.read_csv(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.txt', sep='\t', header=None,names=['sent', 'toks', 'model_prob']).to_dict(orient='records')
+test_data = Path(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.txt').read_text().strip().split('\n')
+test_data = [{'sent':x.split('\t')[0], 'toks': x.split('\t')[1], 'model_prob': x.split('\t')[2]} for x in test_data]
 print(test_data)
 
+model = kenlm.LanguageModel(f'bigram/{grammar_name}_{sample_split}.arpa')
 for sent in test_data:
     toks = sent['toks']
     print(toks)
