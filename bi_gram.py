719228c57a82f5b573932598f974525f86c809a6
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-25 09:47:54 -0400

    add prob code

--- bi_gram.py
+++ bi_gram.py
@@ -2,32 +2,28 @@ import pandas as pd
 import argparse
 import kenlm
 import math
-
-from numpy.distutils.conv_template import header
 from tqdm import tqdm 
-from pathlib import Path
+
+
 ngram = 'bigram'
 parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--grammar_name', type=str, help='name of the grammar, e.g., grammar_close_then_open')
 parser.add_argument('-s', '--sample_split', type=int, help='0-9')
 parser.add_argument('-m', '--model_type', type=str, choices=['trans', 'lstm'])
-parser.add_argument('-a', '--affix', type=str, choices=['reverse', 'permutation', 'r'])
-
 
 args = parser.parse_args()
 model_type = args.model_type
 sample_split = str(args.sample_split)
 grammar_name = args.grammar_name
-affix = args.affix
 
 
 if ngram=='bigram':
     test_data = pd.read_csv(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.txt', sep='\t',
-                            header=None, names=['sent', 'toks', 'model_prob']).to_dict(orient='records')
+                            header=None, names=['sent', 'toks', 'model_prob'], index=False,).to_dict(orient='records')
 elif ngram=='trigram':
-    test_data = pd.read_csv(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.csv', sep=',').to_dict(orient='records')
+    test_data = pd.read_csv(f'{model_type}_sentence_scores/{grammar_name}/{sample_split}.test.csv', index=False, sep=',').to_dict(orient='records')
 
-model = kenlm.LanguageModel(f'{ngram}/{grammar_name}_{sample_split}_{affix}.arpa')
+model = kenlm.LanguageModel(f'{ngram}/{grammar_name}_{sample_split}.arpa')
 for sent in tqdm(test_data):
     toks = sent['toks']
     model_score = model.score(toks, bos=False, eos=False)
--- get_n_gram_prob.sh
+++ get_n_gram_prob.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-all_grammars=("grammar_close_then_open_3" "grammar_close_then_open_30" "grammar_close_then_open_60")
+all_grammars=("grammar_close_then_open_3" "grammar_close_then_open_30" "grammar_close_then_open_60"
+"grammar_close_then_open_3_permutation" "grammar_close_then_open_30_permutation" "grammar_close_then_open_60_permutation"
+ "grammar_close_then_open_3_reverse" "grammar_close_then_open_3_reverse_permutation"
+"grammar_close_then_open_3_r" "grammar_close_then_open_30_r" "grammar_close_then_open_60_r")
+
 mkdir -p bigram
 
 for num in {0..9}; do
   for grammar in "${all_grammars[@]}"; do
-    python bi_gram.py -g "${grammar}" -s $num -m 'trans' -a "permutation"
-    python bi_gram.py -g "${grammar}" -s $num -m 'lstm' -a "permutation"
-    python bi_gram.py -g "${grammar}" -s $num -m 'trans' -a "r"
-    python bi_gram.py -g "${grammar}" -s $num -m 'lstm' -a "r"
-    python bi_gram.py -g "${grammar}" -s $num -m 'trans' -a "reverse"
-    python bi_gram.py -g "${grammar}" -s $num -m 'lstm' -a "reverse"
+    python bi_gram.py -g "${grammar}" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}" -s $num -m 'lstm'
+    python bi_gram.py -g "${grammar}" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}" -s $num -m 'lstm'
+    python bi_gram.py -g "${grammar}" -s $num -m 'trans'
+    python bi_gram.py -g "${grammar}" -s $num -m 'lstm'
   done
 done
