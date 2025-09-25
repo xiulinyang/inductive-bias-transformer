b9603b6e98ad3e4aef59f66a303184108cfaa1b4
xiulinyang <xiulin.yang.compling@gmail.com>
2025-09-24 21:13:34 -0400

    add --discount_fallback

--- train_bygram.sh
+++ train_bygram.sh
@@ -5,11 +5,11 @@ mkdir -p bigram
 
 for num in {0..9}; do
   for grammar in "${all_grammars[@]}"; do
-    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
+    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}/${num}/trn.spm > bigram/${grammar}_${num}.arpa
     kenlm/build/bin/build_binary bigram/${grammar}_${num}.arpa bigram/${grammar}_${num}.bin
 
-    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_permutation/${num}/trn.spm > bigram/${grammar}_${num}_permutation.arpa
-    kenlm/build/bin/lmplz -o 2 -S 80% -T /tmp < data-bin/${grammar}_r/${num}/trn.spm > bigram/${grammar}_${num}_r.arpa
+    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}_permutation/${num}/trn.spm > bigram/${grammar}_${num}_permutation.arpa
+    kenlm/build/bin/lmplz -o 2 -S 80% --discount_fallback -T /tmp < data-bin/${grammar}_r/${num}/trn.spm > bigram/${grammar}_${num}_r.arpa
     kenlm/build/bin/build_binary bigram/${grammar}_${num}_r.arpa bigram/${grammar}_${num}_r.bin
     kenlm/build/bin/build_binary bigram/${grammar}_${num}_permutation.arpa bigram/${grammar}_${num}_permutation.bin
 
